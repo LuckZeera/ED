b4631ffcec04aaaae9bfe55f46a23b6b37e148ca
Lucas de Castro <100042150+LuckZeera@users.noreply.github.com>
2023-02-13 13:46:30 -0300

    Update ordenar.cpp

--- Trabalho Pratico/ordenar.cpp
+++ Trabalho Pratico/ordenar.cpp
@@ -12,30 +12,60 @@ struct Registro { // registro com os campos a serem lidos da base de dados
     // os vetores de char sempre serão inicializados completos por caracteres nulos
 };
 
-void intercala(Registro v[], long long int p,long long  int q, long long int r){
+/*void intercala(Registro a[], int inicio, int meio, int fim) {
+	int i = inicio, j = meio+1;
+	int tamanho = fim - inicio + 1;
+	Registro aux[tamanho]; // vetor auxiliar
+	for (int k=0; k < tamanho; k++) {
+		if ((i <= meio) and (j <= fim)){ 
+			if (strcmp(a[i].Area[0],a[j].Area[0])){ 
+				aux[k] = a[i]; // copia trecho1 em aux[]
+				i++; 			// avança em trecho1
+			}else { // 
+				aux[k] = a[j]; // copia trecho2 em aux[]
+				j++;	 		// avanca em trecho2
+ 			    }
+            }
+            else if(i > meio) { // terminou o trecho1	
+			    aux[k] = a[j];
+			    j++;
+		    }else { 				 // terminou o trecho2
+			    aux[k] = a[i];
+			    i++;
+		}		
+	}	
+	// terminando: copiar de aux[] em a[inicio:fim]
+	for (int k=0; k < tamanho; k++){ 
+		a[inicio + k] = aux[k];
+	}
+}*/
+
+void intercala(Registro v[], long long int p,long long int q,long long int r){
     long long int i = p, j = q;
     long long int tamanho = r - p + 1;
     Registro w[tamanho]; // vetor auxiliar
-    long long int k = 0;
+    int k = 0;
+    int cont = 0;
     while ((i < q) and (j <= r)) {                
-       if (strcpy(v[i].Area,v[j].Area))  {
+       if (strcmp(v[i].Area,v[j].Area))  {
            w[k++] = v[i++]; /* w[k] = v[i]; k++; i++; */
 		} else  {
 			w[k++] = v[j++]; /* w[k] = v[j]; k++; j++; */
 		}
 	} 
-    // terminou um dos vetores, agora copia o outro                           
+// terminou um dos vetores, agora copia o outro                          
     while (i < q) {
         w[k++] = v[i++];
-    }      
+    }   
     while (j <= r) {
        w[k++] = v[j++]; 
     }
     // agora copiamos do vetor auxiliar aux[] em v[p:r]
-	for (long long int m = 0; m < tamanho; m++){ 
+	for (int m = 0; m < tamanho; m++){ 
 		v[p + m] = w[m];
     }
 }
+
 
 void mergesort(Registro a[], long long int inicio, long long int fim){
 	long long int meio;
@@ -88,7 +118,7 @@ void FazerVetor(){
         for (int i = 0; i < length; i++)
             cout << novoRegistro[j].ec_count [i];
         cout << endl;
-    }                  
+    }             
 }
 
 int main(){
